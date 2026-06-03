f3fa5fc70aa3ae3a0fd7b20b836cf57bea899144
adixpergo <aditya.singh@expergo.tech>
2026-06-03 10:28:46 +0530

    deleted dist and node modules

--- backend/dist/config/database.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.toNumber = void 0;
-const toNumber = (value, fallback) => {
-    const parsedValue = Number.parseInt(value ?? '', 10);
-    return Number.isNaN(parsedValue) ? fallback : parsedValue;
-};
-exports.toNumber = toNumber;
-//# sourceMappingURL=database.config.js.map
